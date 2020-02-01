fc4ea7481cddf7bdab5738a2859cc2c35d788df6
Agusia Krzywinska <agusiakrzywinska@gmail.com>
2020-02-01 15:52:27 -0500

    fixed particle system

--- Assets/Scripts/Grabbable.cs
+++ Assets/Scripts/Grabbable.cs
@@ -52,7 +52,7 @@ public class Grabbable : MonoBehaviour
         rb.velocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
         rb.constraints = RigidbodyConstraints.None;
-        GravityGun.instance.ps_spawnObject.transform.position = transform.position;
+        //GravityGun.instance.ps_spawnObject.transform.position = transform.position;
         if (fromDrop)
         {
             GravityGun.instance.audioSource.clip = GravityGun.instance.spawnInSFX;
