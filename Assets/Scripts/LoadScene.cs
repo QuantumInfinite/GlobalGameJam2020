c6e9fe7b565d56684016ac853a1ce074d7c7c96e
Agusia Krzywinska <agusiakrzywinska@gmail.com>
2020-02-01 16:42:27 -0500

    delayed load

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public string stringLevelName;
+    public float timeDelay;
+
+    public void LoadLevel()
+    {
+        StartCoroutine(StartLevel());
+    }
+
+    public IEnumerator StartLevel()
+    {
+        yield return new WaitForSeconds(timeDelay);
+
+        SceneManager.LoadScene(stringLevelName);
+    }
+}
