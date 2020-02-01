88af388aa4727d3868edc8555ecd18a5e748673d
Agusia Krzywinska <agusiakrzywinska@gmail.com>
2020-02-01 16:51:06 -0500

    did some grass offsetting

---
+++ Assets/Scripts/GrassOffset.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassOffset : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<MeshRenderer>().material.SetFloat("_ExtrusionAmount",  Random.Range(17f, 19f));
+    }
+}
