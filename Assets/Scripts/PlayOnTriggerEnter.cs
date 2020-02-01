f2b69e32fc336bef90d33980f320a4aa40661311
Agusia Krzywinska <agusiakrzywinska@gmail.com>
2020-02-01 14:53:59 -0500

    play whatever you want when player enters a trigger

---
+++ Assets/Scripts/PlayOnTriggerEnter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnTriggerEnter : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public ParticleSystem ps;
+    public Animator anim;
+    public string animTrigger;
+    public GameObject myGameObject;
+    public bool turnOffObject;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if(myGameObject)
+            {
+                gameObject.SetActive(turnOffObject);
+            }
+            if (audioSource)
+            {
+                audioSource.Play();
+            }
+            if(ps)
+            {
+                ps.Play();
+            }
+            if(anim)
+            {
+                anim.SetTrigger(animTrigger);
+            }
+        }
+        
+    }
+}
