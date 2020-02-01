5ccceeee57d4d3ae989f0234cff8662ca4725417
Agusia Krzywinska <agusiakrzywinska@gmail.com>
2020-02-01 16:01:02 -0500

    created footsteps swapping script

---
+++ Assets/Scripts/ChangeFootSteps.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class ChangeFootSteps : MonoBehaviour
+{
+    AudioClip[] m_FootstepSounds;
+    AudioClip[] oldFootsteps;
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<FirstPersonController>();
+        if(player)
+        {
+            oldFootsteps = player.m_FootstepSounds;
+            player.m_FootstepSounds = m_FootstepSounds;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var player = other.GetComponent<FirstPersonController>();
+        if (player)
+        {
+            player.m_FootstepSounds = oldFootsteps;
+        }
+    }
+}
