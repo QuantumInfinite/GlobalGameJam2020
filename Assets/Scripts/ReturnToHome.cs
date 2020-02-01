18ff0752e1a53bff3e0981fe2167cb5be642d473
Kyle Jones <Kyle-Jones8D@hotmail.com>
2020-02-01 11:49:08 -0500

    Moved stuff

--- Assets/Scripts/ReturnToHome.cs
+++ Assets/Scripts/ReturnToHome.cs
@@ -4,78 +4,89 @@ using UnityEngine;
 
 public class ReturnToHome : MonoBehaviour
 {
-    public Vector3 origionalPos;
+    Vector3 origionalPos;
     Quaternion origionalRot;
     Vector3 origionalRotation;
-    bool returning = false;
-    float returnSpeed = 5;
-    // Start is called before the first frame update
+
+    public Rigidbody rigid;
+    Collider myCollider;
+    Grabbable grabbable;
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        myCollider = GetComponent<Collider>();
+        grabbable = GetComponent<Grabbable>();
+    }
+
     void Start()
     {
         origionalPos = transform.position;
         origionalRot = transform.rotation;
-        origionalRotation = transform.rotation.eulerAngles;
+        origionalRotation = transform.rotation.eulerAngles;        
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator GoHome(float returnSpeed, float forceEndTime)
     {
-        
-    }
-
-    IEnumerator GoHome()
-    {
-        returning = true;
-        Rigidbody rigid = GetComponent<Rigidbody>();
         rigid.useGravity = false;
-
-        while (returning)
-        {
+        grabbable.enabled = false;
+        //float forceEndTime = Time.realtimeSinceStartup + 2f;
+        bool stillCorrecting = true;
+        while (stillCorrecting && Time.realtimeSinceStartup < forceEndTime)
+        {            
             rigid.velocity = Vector3.zero;
             rigid.angularVelocity = Vector3.zero;
-            Debug.Log(Vector3.Distance(transform.position, origionalPos) + " | " + Quaternion.Angle(transform.rotation, origionalRot));
-            int breaker = 0;
+
+
+
+           // Debug.Log(Vector3.Distance(transform.position, origionalPos) + " | " + Quaternion.Angle(transform.rotation, origionalRot));
+
+
+            stillCorrecting = false;
             if (Vector3.Distance(transform.position, origionalPos) > 0.01)
             {
                 //Movement
                 Vector3 translation = (origionalPos - transform.position) * returnSpeed * Time.deltaTime;
-            
+
                 rigid.MovePosition(transform.position + translation);
-                breaker++;
+
+                stillCorrecting = true;
             }
             if(Quaternion.Angle(transform.rotation, origionalRot) > 20)
             {
                 //Rotation
 
-                Vector3 tran = (origionalRot.eulerAngles - transform.rotation.eulerAngles) * returnSpeed * Time.deltaTime;
+                //Vector3 tran = (origionalRot.eulerAngles - transform.rotation.eulerAngles) * returnSpeed * Time.deltaTime;
 
                 transform.rotation = origionalRot;
-
-
-
-                breaker++;
+                stillCorrecting = true;
             }
-
-            if (breaker == 0)
-            {
-                returning = false;
-            }
-            //Vector3 change = (Vector3.RotateTowards(transform.rotation.eulerAngles, origionalRotation, returnSpeed * 100 * Time.deltaTime, 0.0f));
-            //transform.rotation = Quaternion.Lerp(transform.rotation, origionalRot, Time.time * returnSpeed / 100);
-            //transform.rotation = Quaternion.LookRotation(change);
-            //Debug.Log(Quaternion.RotateTowards(transform.rotation, origionalRot, returnSpeed / 100));
-
-            //rigid.MoveRotation(Quaternion.RotateTowards(transform.rotation, origionalRot, returnSpeed * Time.deltaTime));
 
             yield return new WaitForEndOfFrame();
         }
-        //Debug.Break();
-        GetComponent<Collider>().enabled = false;
-        transform.position = (origionalPos);
+
+        LockDown();
+        
+        yield return null;
+    }
+
+    public void LockDown()
+    {
+        myCollider.enabled = false;
+
+        transform.position = origionalPos;
         transform.rotation = origionalRot;
 
         rigid.isKinematic = true;
         rigid.constraints = RigidbodyConstraints.FreezeAll;
-        yield return null;
+    }
+    
+    public void Unlock()
+    {
+        rigid.useGravity = true;
+        grabbable.enabled = true;
+        myCollider.enabled = true;
+
+        rigid.isKinematic = false;
+        rigid.constraints = RigidbodyConstraints.None;
     }
 }
