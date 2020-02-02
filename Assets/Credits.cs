5b6b95670e10cfb21724d8bd8e68271dc8df7e56
theadamhenderson <theadamhenderson@outlook.com>
2020-02-02 16:13:31 -0500

    GameLopps

---
+++ Assets/Credits.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    public LoadScene scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene.LoadLevel();
+    }
+
+ 
+}
