8bc17226bbfef1f1a558a31609b99e83975e0fea
Kyle Jones <Kyle-Jones8D@hotmail.com>
2020-02-01 19:49:31 -0500

    EE

---
+++ Assets/EasyPositionLink.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasyPositionLink : MonoBehaviour
+{
+    public ReturnToHome[] fracturePieces;
+    public Transform[] fractureTargets;
+    public bool doIt = false;
+
+    private void OnValidate()
+    {
+        if (doIt == false)
+        {
+            return;
+        }
+        doIt = false;
+        if (fracturePieces.Length > 0 && fracturePieces.Length == fractureTargets.Length)
+        {
+            for (int i = 0; i < fracturePieces.Length; i++)
+            {
+                Debug.Log("Setting: " + fracturePieces[i].name + "'s goal to: " + fractureTargets[i].name);
+                fracturePieces[i].goalOverride = fractureTargets[i];
+            }
+        }
+    }
+}
