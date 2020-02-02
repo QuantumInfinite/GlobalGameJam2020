ced1848abf98fb45ba736ef333b6dd7c37164cc9
Kyle Jones <Kyle-Jones8D@hotmail.com>
2020-02-02 15:28:35 -0500

    MadeDisableKin

---
+++ Assets/Scripts/DisableKinimatic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableKinimatic : MonoBehaviour
+{
+    public List<Rigidbody> rigids;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            foreach (var rig in rigids)
+            {
+                rig.isKinematic = false;
+            }
+        }
+    }
+}
